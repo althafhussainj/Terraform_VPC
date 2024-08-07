5cd8b09729e9a2f3723dd7945f1176cdf51473d8
shabana syed <althafhussainj@gmail.com>
2024-08-07 16:29:52 +0530

    vpc.tf updated

--- vpc.tf
+++ vpc.tf
@@ -32,7 +32,7 @@ resource "aws_route_table" "PublicRT" {
   vpc_id = aws_vpc.myvpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
+    gateway_id = aws_internet_gateway.IGW.id
   }
   tags = {
     Name = "MyRoute"
