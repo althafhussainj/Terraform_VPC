1382fd492b7a850e2cc7a79e3bf898f2dfd2484d
shabana syed <althafhussainj@gmail.com>
2024-08-07 11:51:49 +0530

    creating vpc

---
+++ vpc.tf
@@ -0,0 +1,40 @@
+# Creating VPC
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "MyterraformVPC"
+  }
+}
+
+# Creating Public Subnet
+resource "aws_subnet" "publicsubnet" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = "10.0.1.0/24"
+}
+
+# Creating Private Subnet
+resource "aws_subnet" "privatesubnet" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = "10.0.2.0/24"
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "IGW" {
+  vpc_id = aws_vpc.myvpc.id
+}
+
+# Create Route Table
+resource "aws_route_table" "PublicRT" {
+  vpc_id = aws_vpc.myvpc.id
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.IGW.id
+  }
+}
+
+# Create route table association 
+resource "aws_route_table_association" "publicRTassociation"{
+    route_table_id = aws_route_table.id 
+    subnet_id = aws_subnet.publicsubnet.id
+}
+
