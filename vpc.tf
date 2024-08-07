c43533bad52db4bc983bfd91e1e7ab32339545eb
shabana syed <althafhussainj@gmail.com>
2024-08-07 12:18:33 +0530

    vpc.tf  modified

--- vpc.tf
+++ vpc.tf
@@ -27,14 +27,16 @@ resource "aws_internet_gateway" "IGW" {
 resource "aws_route_table" "PublicRT" {
   vpc_id = aws_vpc.myvpc.id
   route = {
-    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.IGW.id
+    cidr_block = "0.0.0.0/0"
+    
   }
 }
 
 # Create route table association 
 resource "aws_route_table_association" "publicRTassociation"{
-    route_table_id = aws_route_table.id 
     subnet_id = aws_subnet.publicsubnet.id
+    route_table_id = aws_route_table.id 
+    
 }
 
