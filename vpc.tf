87081d23c54d503dfb3155076d63929ff50f8290
shabana syed <althafhussainj@gmail.com>
2024-08-07 16:32:21 +0530

    vpc.tf updated

--- vpc.tf
+++ vpc.tf
@@ -43,7 +43,7 @@ resource "aws_route_table" "PublicRT" {
 # Create route table association 
 resource "aws_route_table_association" "publicRTassociation"{
     subnet_id = aws_subnet.publicsubnet.id
-    route_table_id = aws_route_table.id 
+    route_table_id = aws_route_table.PublicRT.id
     
 }
 
